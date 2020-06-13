12fa8d7b276e00c625eb81cb40e22686240afe8f
frongrider2 <frongrider2@gmail.com>
2020-06-13 13:06:50 +0700

    Initial commit

--- script/swip.js
+++ script/swip.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper1 = new Swiper('.swiper1', {
   effect: 'coverflow',
   grabCursor: true,
   centeredSlides: true,
@@ -11,6 +11,29 @@ var swiper = new Swiper('.swiper-container', {
     slideShadows : false,
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination1',
   },
+});
+
+var swiper2 = new Swiper('.swiper2', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: false,
+  slidesPerView: 'auto',
+  loop: true,
+  loopFillGroupWithBlank: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows : false,
+  },
+  pagination: {
+    el: '.swiper-pagination2',
+  },
+  navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+},
 });
